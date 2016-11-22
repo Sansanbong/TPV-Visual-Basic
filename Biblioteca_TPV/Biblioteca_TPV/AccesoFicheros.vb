d5238da2f73956c30414130c3450c143d3a0d3ac
geraldoriviarino <mariogarciarb@gmail.com>
2016-11-22 20:55:27 +0100

    Add files via upload
    Añadido el método crearFicheroUsuario que crea el fichero usuarios.txt con un admin en caso de que no exista.

--- Biblioteca_TPV/Biblioteca_TPV/AccesoFicheros.vb
+++ Biblioteca_TPV/Biblioteca_TPV/AccesoFicheros.vb
@@ -255,6 +255,31 @@ Namespace ficheros
 
         End Function
 
+        Public Sub crearFicheroUsuarios()
+            Dim estructura As New Usuario.stUsuario
+
+
+            If Not My.Computer.FileSystem.FileExists(PATH_USUARIOS) Then
+
+                estructura.nick = "admin"
+                estructura.contrasenya = "admin"
+                estructura.dni = "53848813V"
+                estructura.nombreEmpleado = "Mario Javier"
+                estructura.rol = 1
+                estructura.telefono = 691582472
+                estructura.apellidos = "Sánchez Ramos"
+                estructura.correo = "soyeladminytuno@jeje.com"
+
+
+                FileOpen(FICH_USUARIOS, PATH_USUARIOS, OpenMode.Random, OpenAccess.Write, , Len(estructura))
+
+                FilePut(FICH_USUARIOS, estructura, 1)
+                FileClose(FICH_USUARIOS)
+            End If
+
+
+        End Sub
+
 
         '--------------GESTIÓN PRODUCTOS---------------
         'Lectura de todos los productos
@@ -315,10 +340,7 @@ Namespace ficheros
                         posicion = posicion + 1
                         If estructura.id.Trim = id Then
                             FileClose(FICH_PRODUCTOS)
-                            Dim producto As New Producto(estructura.id.Trim,
-                                              estructura.nombre.Trim,
-                                              estructura.precio,
-                                              estructura.categoria.Trim)
+                            Dim producto As New Producto(estructura)
                             Return producto
                         End If
                     End While
